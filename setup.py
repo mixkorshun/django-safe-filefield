6877ab57f85753ae8ea88acc3c2e00ff4d0c6382
Vladislav Bakin <mixkorshun@gmail.com>
2017-10-09 02:28:45 +0300

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-safe-filefield',
-    version='0.2.0',
+    version='0.3.0',
     url='https://github.com/mixkorshun/django-safe-filefield',
     description='Secure file field, which allows you to '
                 'restrict uploaded file extensions.',
